ddad1e7dc84384bd5e7b58bdd13e542dd8dddd40
PashaBalandin <pashabalandin1200@gmail.com>
2021-07-18 17:47:27 +0300

    Status code has been changed

--- src/main/java/com/provider/exception/ResourcesAlreadyExistsException.java
+++ src/main/java/com/provider/exception/ResourcesAlreadyExistsException.java
@@ -3,6 +3,6 @@ package com.provider.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NO_CONTENT)
+@ResponseStatus(HttpStatus.CONFLICT)
 public class ResourcesAlreadyExistsException extends RuntimeException {
 }
