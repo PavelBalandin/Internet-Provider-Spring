0390452e3f90a2f0ca0d556ab8ab6bebdfdcddc1
PashaBalandin <pashabalandin1200@gmail.com>
2021-08-26 14:13:31 +0300

    Test for tariff controller has been changed

--- src/test/java/com/provider/controller/TariffControllerTest.java
+++ src/test/java/com/provider/controller/TariffControllerTest.java
@@ -7,14 +7,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TariffController.class) //Annotation that can be used for a Spring MVC test that focuses only on Spring MVC components.
@@ -31,10 +35,17 @@ class TariffControllerTest {
 
     @Test
     void getAll() throws Exception {
-        List<TariffDto> tariffDtoList = new ArrayList<>();
-        when(tariffService.getAll()).thenReturn(tariffDtoList);
+        List<TariffDto> expected = new ArrayList<>();
+        expected.add(TariffDto.builder().id(1L).name("name").description("description").duration(30).price(BigDecimal.valueOf(100)).build());
+        Page<TariffDto> tariffDtoPage = new PageImpl<TariffDto>(expected);
+        when(tariffService.getAll(0, 5, "id", "ASC")).thenReturn(tariffDtoPage);
 
         mockMvc.perform(get("/api/v1/tariffs"))
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.content.[0].id").value("1"))
+                .andExpect(jsonPath("$.content.[0].name").value("name"))
+                .andExpect(jsonPath("$.content.[0].description").value("description"))
+                .andExpect(jsonPath("$.content.[0].duration").value("30"))
+                .andExpect(jsonPath("$.content.[0].price").value("100"));
     }
 }
